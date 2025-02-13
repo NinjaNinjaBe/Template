7abdce55d5c5fd5381ba8a6ee536430fddc0d859
Killian Pieyns <85182700+killianpieyns@users.noreply.github.com>
2025-02-14 00:47:23 +0100

    refactor: improve formatting and consistency in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,23 +7,25 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: "*",
+      origin: '*',
       // origin: process.env.NODE_ENV === 'development' ? '*' : process.env.FRONTEND_URL,
     },
     rawBody: true,
-    logger: ['error', 'warn', 'log', 'debug', 'verbose']
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   // app.useGlobalFilters(
   //   new AllExceptionsFilter()
   // );
-  
+
   if (process.env.NODE_ENV === 'development') {
     const configSwagger = new DocumentBuilder()
       .setTitle('DEFAULT APP NAME')
