88cbca56848c050a7eb41c72ed5e32db57745e6b
Killian Pieyns <85182700+killianpieyns@users.noreply.github.com>
2025-02-14 02:31:52 +0100

    feat: integrate Clerk authentication and update environment configuration

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,9 +1,26 @@
 <template>
     <div>
-        <slot />
+        <header>
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+        </header>
+
+        <main>
+            <slot />
+        </main>
+
+        <footer>
+            <p>&copy; {{ currentYear }}</p>
+        </footer>
     </div>
 </template>
 
 <script setup lang="ts">
+
+const currentYear = new Date().getFullYear();
 
 </script>
